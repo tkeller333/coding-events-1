1c2379dcb01e572ca70e340a2b58ddab3e751138
Teresa Keller <56941889+tkeller333@users.noreply.github.com>
2020-06-24 16:32:09 -0500

    completed exercises

---
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -0,0 +1,35 @@
+package org.launchcode.codingevents.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class EventCategory {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotBlank(message = "Please enter a name.")
+    private String name;
+
+    public EventCategory(String name) {
+        this.name = name;
+    }
+
+    public EventCategory() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
